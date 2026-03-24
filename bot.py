03535d13a3b704b398e34972736715b95a3d5dd7
ipun-tech <136713314+ipun-tech@users.noreply.github.com>
2026-03-24 19:41:04 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,5 +117,5 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # ===== RUN =====
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT, handle))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
 app.run_polling()
